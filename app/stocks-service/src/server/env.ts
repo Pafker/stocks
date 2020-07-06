1e1c775486dd2218507ad52d99ef93db65e6439b
Pafker <ppatrix555@gmail.com>
2020-07-06 12:31:59 +0200

    Added mongo container

--- app/stocks-service/src/server/env.ts
+++ app/stocks-service/src/server/env.ts
@@ -1,5 +1,8 @@
 import { config } from '../../deps.ts';
 
-type EnvironmentVariables = { MQ_URL: string };
+interface EnvironmentVariables {
+    MQ_URL: string;
+    DB_URL: string;
+}
 
 export const ENV = config({ safe: true }) as EnvironmentVariables;
