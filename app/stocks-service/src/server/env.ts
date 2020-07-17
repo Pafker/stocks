049326d1a89193553c9d35d27b06e9f58bea5c00
Pafker <ppatrix555@gmail.com>
2020-07-17 15:15:23 +0200

    Changed mongodb for persistent redis, since mongo has connection problems with deno docker https://github.com/denoland/deno/issues/5888

--- app/stocks-service/src/server/env.ts
+++ app/stocks-service/src/server/env.ts
@@ -2,7 +2,7 @@ import { config } from '../../deps.ts';
 
 type EnvironmentVariables = { 
     MQ_URL: string;
-    DB_URL: string;
+    REDIS_URL: string;
 };
 
 export const ENV = config({ safe: true }) as EnvironmentVariables;
